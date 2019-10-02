34fb43072c7c9fb45fafcab1e7b8f9b8558d586f
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-10-02 22:36:56 +0530

    initial implementation of reader writier lock comnpleted

--- Makefile
+++ Makefile
@@ -4,19 +4,17 @@ LIB_DIR=./lib
 SRC_DIR=./src
 BIN_DIR=./bin
 
-CUTLERY_PATH=../cutlery
-
 CC=gcc
 RM=rm -f
 
-TARGET=libboompar.a
+TARGET=librwlock.a
 
-CFLAGS=-I${INC_DIR} -I${CUTLERY_PATH}/inc
+CFLAGS=-I${INC_DIR}
 
-${OBJ_DIR}/%.o : ${SRC_DIR}/%.c ${INC_DIR}/%.h ${CUTLERY_PATH}/inc
+${OBJ_DIR}/%.o : ${SRC_DIR}/%.c ${INC_DIR}/%.h
 	${CC} ${CFLAGS} -c $< -o $@
 
-${BIN_DIR}/$(TARGET) : ${OBJ_DIR}/job.o ${OBJ_DIR}/jobstatus.o ${OBJ_DIR}/executor.o
+${BIN_DIR}/$(TARGET) : ${OBJ_DIR}/rwlock.o
 	ar rcs $@ ${OBJ_DIR}/*.o 
 
 all: ${BIN_DIR}/$(TARGET)
---
+++ inc/rwlock.h
@@ -0,0 +1,40 @@
+#ifndef RW_LOCK_H
+#define RW_LOCK_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<pthread.h>
+
+typedef struct rwlock rwlock;
+struct rwlock
+{
+	unsigned int reader_threads_waiting;
+	unsigned int reading_threads;
+
+	unsigned int writer_threads_waiting;
+	unsigned int writing_threads;
+
+	pthread_mutex_t internal_protector;
+
+	pthread_mutex_t protector;
+
+	pthread_cond_t read_wait;
+	pthread_cond_t write_wait;
+};
+
+rwlock* get_rwlock();
+
+void read_lock(rwlock* rwlock_p);
+
+void read_unlock(rwlock* rwlock_p);
+
+void write_lock(rwlock* rwlock_p);
+
+void write_unlock(rwlock* rwlock_p);
+
+// if some one is already holding a read or write lock, we can not delete the lock
+// if we could not delete the lock, it function returns -1, else 0
+int delete_rwlock(rwlock* rwlock_p);
+
+#endif
