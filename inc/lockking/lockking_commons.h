6a070ad276c0702960fc2405c378a36325a90e2f
RohanVDvivedi <rohandvivedi@gmail.com>
2025-05-21 22:51:19 +0530

    brought blocking and non-blocking macros out into a separate file

---
+++ inc/lockking/lockking_commons.h
@@ -0,0 +1,8 @@
+#ifndef LOCKKING_COMMONS_H
+#define LOCKKING_COMMONS_H
+
+// below are the 2 special parameters of the uint64_t timeout_in_microseconds, parameter for use in the below functions
+#define NON_BLOCKING   UINT64_C(0)
+#define BLOCKING       UINT64_MAX
+
+#endif
